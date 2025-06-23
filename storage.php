d9a92bc707428b5bd2d284758295bf013b3884ec
cefireazure2505 <cefire.azure.2505@gmail.com>
2025-06-23 16:36:02 +0200

    Update storage.php

--- storage.php
+++ storage.php
@@ -1,11 +1,10 @@
 <?php
 
+require __DIR__ . '/vendor/autoload.php';
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-
-require "vendor/autoload.php";
 
 use MicrosoftAzure\Storage\Blob\BlobRestProxy;
 use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
